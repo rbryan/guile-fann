00f98ff45697dff9be2292143dc3a02b64c64519
Russell Bryan <bryanr@ou.edu>
2016-04-28 20:48:17 -0500

    made a fann/train.scm

--- fann/fann.scm
+++ fann/fann.scm
@@ -1,11 +1,11 @@
 (define-module (fann fann)
 	       #:use-module (system foreign)
 	       #:use-module (debugging assert) ;;adds guile-lib as a dependency
-	       #:export (fann-create-network))
+	       #:export (create-network))
 
 (define libfann (dynamic-link "libfann"))
 
-(define* (fann-create-network type . args)
+(define (create-network type . args)
 	 (cond
 	   ((equal? type 'standard)
 	    	(let ((largs (length args)))
@@ -30,4 +30,5 @@
 				      (append (list float) (make-list (- largs 1) uint32))) args)))
 	   (else
 	     	(error "Unknown type for new network."))))
+
 
